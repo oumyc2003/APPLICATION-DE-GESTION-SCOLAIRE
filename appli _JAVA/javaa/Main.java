820b951e905d52d87ee6aaccd3bd3c37728fedcf
oumyc2003 <151852769+oumyc2003@users.noreply.github.com>
2024-04-29 00:04:02 +0000

    Add files via upload

---
+++ appli _JAVA/javaa/Main.java
@@ -0,0 +1,22 @@
+package javaa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+   /*     // Connexion à la base de données
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/votre_base_de_donnees", "utilisateur", "mot_de_passe")) {
+            // Instanciation des contrôleurs
+            CoursController controleurCours = new CoursController(connection);
+            EtudiantController controleurEtudiant = new EtudiantController (connection);
+               // Ajout d'un cours
+        }
+           
+    }
+           
+}        
+        //?????? probleme de try catch// */ 
+    }}
